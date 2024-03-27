571af7e1138da3425e6aae6c8ebe1c0fd5590c07
Rafael Silva <raffael.siilva.00739@gmail.com>
2024-03-27 16:56:53 -0300

    Exercio modulo 08

---
+++ TarefaMod08/src/Calculo.java
@@ -0,0 +1,19 @@
+public class Calculo {
+
+    public static void main(String args[]){
+        calculo1();
+    }
+
+    private static void calculo1() {
+        System.out.println("*****CALCULO E DIVISÃO*****");
+
+        int num1 = 8390;
+        int num2 = 150;
+        int num3 = 210;
+        int num4 = 365;
+
+        int num5 = (num1 + num2 + num3 + num4) / 4;
+        System.out.println("A média dos 4 números é: " + num5);
+    }
+
+}
